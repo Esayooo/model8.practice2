c2d7b930cf50049c86ca61e10112155a2544a2b2
Esayooo <yercanat@gmail.com>
2023-11-09 17:20:13 +0600

    Добавьте файлы проекта.

---
+++ model8.practice2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model8.practice2
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }  
+        public int Discount { get; set; }   
+        public int category { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Supermarket  supermarket = new Supermarket();
+            Console.WriteLine("Сумма всех продуктов :[0]", supermarket[1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ model8.practice2/Supermarket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model8.practice2
+{
+    class Supermarket
+    {
+        List<Product> products = null;
+        public Supermarket() 
+        { 
+            products = new List<Product>();
+            products.Add(new Product { Price = 1000,category= 1 });
+            products.Add(new Product { Price=2000, category = 1 });
+
+        }
+
+        public double this[int category]
+        {
+            get
+            {
+                TimeSpan startTime = new TimeSpan(8,0,0);
+                TimeSpan endDay = new TimeSpan(12,0,0);
+                var t=DateTime.Now.TimeOfDay;
+                double sum = 0;
+                foreach (Product item in products)
+                {
+                    //sum =  sum + item.Price;
+                    sum += item.Price;
+                }
+                if (t>startTime && t < endDay)
+                { 
+                    return sum * 0.95;
+                }
+                else
+                {
+                    return sum;
+                }
+            }
+        }
+    }
+}
